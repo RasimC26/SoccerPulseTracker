ba66b6723a61093fa2eaac77c0967090329af387
RasimC26 <rasimcrnica@gmail.com>
2026-03-18 22:51:06 -0600

    Set up collector.py to listen on a real twitch stream, read the amount of chats per second and write it to a csv.

--- collector.py
+++ collector.py
@@ -1,7 +1,8 @@
 import socket
-import logging
 import os
 import requests
+import time
+import csv
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,15 +11,53 @@ CLIENT_ID = os.getenv("TWITCH_CLIENT_ID")
 CLIENT_SECRET = os.getenv("TWITCH_CLIENT_SECRET")
 NICK = os.getenv("TWITCH_NICK")
 CHAN = os.getenv("TWITCH_CHANNEL")
+PASS = os.getenv("TWITCH_TOKEN")
 
-def get_oauth_token():
-    url = "https://id.twitch.tv/oauth2/token"
-    params = {
-        "client_id": CLIENT_ID,
-        "client_secret": CLIENT_SECRET,
-        "grant_type": "client_credentials"
-    }
-    response = requests.post(url, params=params)
-    data = response.json()
-    return data.get("access_token")
 
+def main():
+
+    # Connect to server and authenticate
+    s = socket.socket()
+    s.connect(("irc.chat.twitch.tv", 6667))
+    s.send(f"PASS {PASS}\r\n".encode("utf-8"))
+    s.send(f"NICK {NICK}\r\n".encode("utf-8"))
+    s.send(f"JOIN {CHAN}\r\n".encode("utf-8"))
+
+    print(f"Connected to {CHAN}. Listening...")
+
+    s.recv(2048)
+
+    # counters
+    message_count = 0
+    last_check_time = time.time()
+
+    # Prepare CSV file
+    with open('pulse_data.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(['timestamp', 'message-count']) # Header for CSV file
+
+    while True:
+        # Listens for data
+        response = s.recv(2048).decode("utf-8")
+        
+        if response.startswith("PING"):
+            s.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
+        elif "PRIVMSG" in response:
+            message_count += 1 
+
+        # Check if 1 second (will change to 1 min in future) has passed
+        current_time = time.time()
+        if current_time - last_check_time >= 1:
+            # Save the "Pulse" to our CSV
+            with open('pulse_data.csv', 'a', newline='') as f:
+                writer = csv.writer(f)
+                writer.writerow([time.strftime('%H:%M:%S'), message_count])
+            
+            print(f"Pulse: {message_count} messages/sec") 
+            
+            # Reset for the time measurement 
+            message_count = 0
+            last_check_time = current_time
+
+if __name__ == "__main__":
+    main()
